d32a28682b71a3cb069664be1c3b92d69f7176b2
jijae92 <jijae92@gmail.com>
2023-03-08 21:48:53 +0900

    게시물 리스팅, 추가 기능 구현

--- src/App.java
+++ src/App.java
@@ -1,15 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public void run() {
         Scanner sc = Container.scanner;
+        int articleLastId = 0;
+        List<Article> articles = new ArrayList<>();
 
         while (true) {
             System.out.printf("명령어) ");
             String cmd = sc.nextLine();
+            if(cmd.equals("article add")){
+                System.out.println("== 게시물 생성 ==");
+                int id = articleLastId + 1;
+                articleLastId++;
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
 
-            if(cmd.equals("article list")) {
+                Article article = new Article(id,title,body);
+                articles.add(article);
+
+                System.out.println("생성된 게시물 객체 : " + article);
+                System.out.printf("%d번 게시물을 생성하였습니다.\n", id);
+            }
+            else if(cmd.equals("article list")) {
+                if(articles.isEmpty()){
+                    System.out.println("게시물이 존재하지 않습니다.");
+                    continue;
+                }
+
                 System.out.println("== 게시물 리스트 ==");
+                        
+
+                System.out.println("번호 / 제목");
+                for(Article article : articles){
+                    System.out.printf("%d / %s\n", article.id, article.title);
+                }
             } else if (cmd.equals("system exit")) {
                 System.out.println("시스템 종료");
                 break;
---
+++ src/Article.java
@@ -0,0 +1,16 @@
+public class Article {
+    public int id;
+    public String title;
+    public String body;
+
+    public Article(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{id: %d, title : \"%s\", body : \"%s\"}", id,title,body);
+    }
+}
