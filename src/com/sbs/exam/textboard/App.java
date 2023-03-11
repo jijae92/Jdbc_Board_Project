4f14100be707ad753482bb0aeaa24b9aed72465e
jijae92 <jijae92@gmail.com>
2023-03-11 21:22:02 +0900

    게시물 리스트 데이터를 DB에서 가져오기

--- src/com/sbs/exam/textboard/App.java
+++ src/com/sbs/exam/textboard/App.java
@@ -1,9 +1,6 @@
 package com.sbs.exam.textboard;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,7 +9,8 @@ public class App {
     public void run() {
         Scanner sc = Container.scanner;
         int articleLastId = 0;
-        List<Article> articles = new ArrayList<>();
+
+
 
         while (true) {
             System.out.printf("명령어) ");
@@ -25,8 +23,6 @@ public class App {
                 System.out.printf("내용 : ");
                 String body = sc.nextLine();
 
-                int id = articleLastId + 1;
-                articleLastId++;
 
                 Connection conn = null;
 
@@ -48,9 +44,6 @@ public class App {
                     pstat = conn.prepareStatement(sql);
                     int affectedRows = pstat.executeUpdate();
 
-                    System.out.println("affectedRows : " + affectedRows);
-
-                    System.out.println(sql);
 
                 }
                 catch(ClassNotFoundException e){
@@ -78,13 +71,80 @@ public class App {
                     }
                 }
 
-                Article article = new Article(id,title,body);
-                articles.add(article);
-
-                System.out.println("생성된 게시물 객체 : " + article);
-                System.out.printf("%d번 게시물을 생성하였습니다.\n", id);
             }
             else if(cmd.equals("article list")) {
+
+                Connection conn = null;
+                ResultSet rs = null;
+                PreparedStatement pstat = null;
+
+                List<Article> articles = new ArrayList<>();
+
+                try{
+                    Class.forName("com.mysql.jdbc.Driver");
+
+                    String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+                    conn = DriverManager.getConnection(url, "jijae92", "tiger");
+
+                    String sql = "SELECT *" ;
+                    sql +=       " FROM article" ;
+                    sql +=       " ORDER BY id DESC;" ;
+
+                    pstat = conn.prepareStatement(sql);
+                    rs = pstat.executeQuery();
+
+                    while (rs.next()){
+                        int id = rs.getInt("id");
+
+                        String regDate = rs.getString("regDate");
+                        String updateDate = rs.getString("updateDate");
+                        String title = rs.getString("title");
+                        String body = rs.getString("body");
+
+                        Article article = new Article(id, regDate, updateDate, title, body);
+                        articles.add(article);
+
+                    }
+
+
+                }
+                catch(ClassNotFoundException e){
+                    System.out.println("드라이버 로딩 실패" + e);
+                }
+                catch(SQLException e){
+                    System.out.println("에러: " + e);
+                }
+                finally{
+
+                    try{
+                        if(rs != null && !rs.isClosed()){
+                            rs.close();
+                        }
+                    }catch (SQLException e){
+                        e.printStackTrace();
+                    }
+
+                    try{
+                        if(pstat != null && !pstat.isClosed()){
+                            pstat.close();
+                        }
+                    }catch (SQLException e){
+                        e.printStackTrace();
+                    }
+
+                    try{
+                        if( conn != null && !conn.isClosed()){
+                            conn.close();
+                        }
+                    }
+                    catch( SQLException e){
+                        e.printStackTrace();
+                    }
+
+
+                }
+
                 if(articles.isEmpty()){
                     System.out.println("게시물이 존재하지 않습니다.");
                     continue;
