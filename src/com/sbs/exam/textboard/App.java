79c380243d9b8a05e265baa07344171cd3571f77
jijae92 <jijae92@gmail.com>
2023-03-13 19:09:36 +0900

    회원 가입시 로그인 아이디 중복체크

--- src/com/sbs/exam/textboard/App.java
+++ src/com/sbs/exam/textboard/App.java
@@ -68,6 +68,20 @@ public class App {
                     System.out.println("로그인 아이디를 입력해주세요.");
                     continue;
                 }
+                SecSql sql = new SecSql();
+
+                sql.append("SELECT COUNT(*) > 0");
+                sql.append("FROM `member`");
+                sql.append("WHERE loginId = >", loginId);
+
+                boolean loginedDup = DBUtil.selectRowBooleanValue(conn, sql);
+                if (loginedDup){
+                    System.out.printf("%s는 이미 사용중인 로그인 아이디입니다.\n", loginId);
+                    continue;
+                }
+
+
+
                 break;
             }
             while(true){
